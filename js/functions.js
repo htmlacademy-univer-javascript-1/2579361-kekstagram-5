a2e112f4ed6d7026836ddfeb2c141cd67a7070c5
Danil Lavrentev <116004920+danillavrentev@users.noreply.github.com>
2024-11-21 20:44:51 +0700

    Fix 2

--- js/functions.js
+++ js/functions.js
@@ -23,42 +23,41 @@
 // console.log(strCheck2('проверяемая строка',10));
 
 
-// Функция для проверки, является ли строка палиндромом
+//Функция для проверки, является ли строка палиндромом
 
-// let str;
-// const isPalindrom = function(str) {
+let str;
+const isPalindrom = function(str) {
 
-//   return str.toUpperCase().split('').reverse().join('') === str.toUpperCase().split('').join('') ? 'Эта строка палиндром': 'Не является палиндромом';
+  return str.toUpperCase().replaceAll(' ','').split('').reverse().join('') === str.toUpperCase().replaceAll(' ','').split('').join('') ? 'Эта строка палиндром': 'Не является палиндромом';
 
-// }
-// console.log(isPalindrom('топот'));
-// console.log(isPalindrom('ДовОд'));
-// console.log(isPalindrom('Кекс'));
-
-
-// Функция принимает строку, извлекает содержащиеся в ней цифры от 0 до 9 и возвращает их в виде целого положительного числа.
-// Если в строке нет ни одной цифры, функция должна вернуть NaN:
-
-
-function extractDigits(string) {
-  let digits = '';
-
-  for (let i = 0; i < string.length; i++) {
-    const character = string[i];
-
-    if (character >= '0' && character <= '9') {
-      digits += character;
-    }
-  }
-
-  return digits.length > 0 ? parseInt(digits, 10) : NaN;
 }
+console.log(isPalindrom('топот шалаш'));
+console.log(isPalindrom('ДовОд'));
+console.log(isPalindrom('Кекс '));
 
-// Пример использования
-extractDigits('2023 год'); // 2023
-extractDigits('ECMAScript 2022'); // 2022
-extractDigits('1 кефир, 0.5 батона'); // 105
-extractDigits('агент 007'); // 7
-extractDigits('а я томат'); // NaN
+
+//Функция принимает строку, извлекает содержащиеся в ней цифры от 0 до 9 и возвращает их в виде целого положительного числа.
+//Если в строке нет ни одной цифры, функция должна вернуть NaN:
+
+
+// function extractDigits(string) {
+//   let digits = '';
+
+//   for (let i = 0; i < string.length; i++) {
+//     const character = string[i];
+
+//     if (character >= '0' && character <= '9') {
+//       digits += character;
+//     }
+//   }
+//   return digits.length > 0 ? parseInt(digits, 10) : NaN;
+// }
+
+// // Пример использования
+// extractDigits('2023 год'); // 2023
+// extractDigits('ECMAScript 2022'); // 2022
+// extractDigits('1 кефир, 0.5 батона'); // 105
+// extractDigits('агент 007'); // 7
+// extractDigits('а я томат'); // NaN
 
 

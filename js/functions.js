b29bbe285e9647a74c84285554c7c60ba6da5692
Danil Lavrentev <danil-lavrentev-2000@mail.ru>
2024-11-25 21:15:18 +0700

    Правки

--- js/functions.js
+++ js/functions.js
@@ -1,39 +1,35 @@
 // Функция для проверки длины строки
 
-let str, result;
 const isStrLengthValid = function (str) {
-  return (result = str.length > 20 ? true : false);
+  return str.length > 20;
 };
 
-console.log(isStrLengthValid("проверяемая строка"));
+console.log(isStrLengthValid("проверяемая строка")); // false
 
 // Другое решение
 
-let newStr, length;
-const isStrLengthValid2 = function (str, length) {
-  return str.length <= length;
+const isStrLengthValid2 = function (str, maxLength) {
+  return str.length <= maxLength;
 };
 
 console.log(isStrLengthValid2("проверяемая строка", 20));
 console.log(isStrLengthValid2("проверяемая строка", 18));
 console.log(isStrLengthValid2("проверяемая строка", 10));
 
-//Функция для проверки, является ли строка палиндромом
+// Функция для проверки, является ли строка палиндромом
 
 const isPalindrom = function (palindromStr) {
   const normalizedStr = palindromStr.toUpperCase().replaceAll(" ", "");
-
   const reversedStr = normalizedStr.split("").reverse().join("");
-
-  return reversedStr === normalizedStr;
+  return reversedStr === normalizedStr; // Если равны, вернется true, иначе false
 };
 
 console.log(isPalindrom("топот шалаш"));
 console.log(isPalindrom("ДовОд"));
 console.log(isPalindrom("Кекс "));
 
-//Функция принимает строку, извлекает содержащиеся в ней цифры от 0 до 9 и возвращает их в виде целого положительного числа.
-//Если в строке нет ни одной цифры, функция должна вернуть NaN:
+// Функция принимает строку, извлекает содержащиеся в ней цифры от 0 до 9 и возвращает их в виде целого положительного числа.
+// Если в строке нет ни одной цифры, функция должна вернуть NaN:
 
 function extractDigits(string) {
   let digits = "";
@@ -42,15 +38,16 @@ function extractDigits(string) {
     const character = string[i];
 
     if (character >= "0" && character <= "9") {
-      digits += character;
+      digits += character; // Собираем найденные цифры
     }
   }
-  return digits.length > 0 ? parseInt(digits, 10) : NaN;
+
+  return digits.length > 0 ? parseInt(digits, 10) : NaN; // Возвращаем число или NaN
 }
 
 // Пример использования
-extractDigits("2023 год"); // 2023
-extractDigits("ECMAScript 2022"); // 2022
-extractDigits("1 кефир, 0.5 батона"); // 105
-extractDigits("агент 007"); // 7
-extractDigits("а я томат"); // NaN
+console.log(extractDigits("2023 год")); // 2023
+console.log(extractDigits("ECMAScript 2022")); // 2022
+console.log(extractDigits("1 кефир, 0.5 батона")); // 105
+console.log(extractDigits("агент 007")); // 7
+console.log(extractDigits("а я томат")); // NaN

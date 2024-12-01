8b963ecc9fbf672767fc35a539ff7c8cfdb00c7d
Danil Lavrentev <danil-lavrentev-2000@mail.ru>
2024-12-01 14:15:41 +0700

    Исправление замечаний

--- js/main.js
+++ js/main.js
@@ -1,16 +1,15 @@
-
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function getRandomComments() {
+function getRandomCommentMessage() {
   const commentsList = [
-    'Всё отлично!',
-    'В целом всё неплохо. Но не всё.',
-    'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-    'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-    'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-    'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+    "Всё отлично!",
+    "В целом всё неплохо. Но не всё.",
+    "Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.",
+    "Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.",
+    "Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.",
+    "Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!",
   ];
 
   const selectedComments = [];
@@ -24,40 +23,70 @@ function getRandomComments() {
     }
   }
 
-  return selectedComments.join(' ');
+  return selectedComments.join(" ");
 }
 
+function getRandomName() {
+  const names = [
+    "Анна",
+    "Иван",
+    "Мария",
+    "Дмитрий",
+    "Ольга",
+    "Алексей",
+    "Екатерина",
+    "Сергей",
+    "Елена",
+    "Андрей",
+    "Татьяна",
+    "Михаил",
+    "Юлия",
+    "Павел",
+    "Наталья",
+    "Владимир",
+    "Ксения",
+    "Николай",
+    "Ирина",
+    "Александр",
+  ];
+  return names[getRandomInt(0, names.length - 1)];
+}
+
+// Функция для создания комментариев
+function createComments(photoId, commentIndex) {
+  return {
+    id: commentIndex + 1 + (photoId - 1) * 30,
+    avatar: `img/avatar-${getRandomInt(1, 6)}.svg`,
+    message: getRandomCommentMessage(),
+    name: getRandomName(),
+  };
+}
+
+// Функция для создания данных фотографий
 function createPhotoObjects() {
   const photos = [];
-
-  // Создаем 10 объектов фотографий для примера
   for (let i = 1; i <= 25; i++) {
+    const commentCount = getRandomInt(0, 30);
+    const comments = [];
+
+    // Генерируем комментарии прямо здесь
+    for (let j = 0; j < commentCount; j++) {
+      const comment = createComments(i, j);
+      comments.push(comment);
+    }
+
     const photo = {
       id: i,
-      url: `url_to_photo_${i}.jpg`,
+      url: `photos/${i}.jpg`,
       description: `Описание фотографии номер ${i}: здесь можно увидеть что-то интересное!`,
       likes: getRandomInt(15, 200),
-      comments: []
+      comments: comments, // Добавляем сгенерированные комментарии
     };
 
-    const commentCount = getRandomInt(0, 30);
-
-    for (let j = 0; j < commentCount; j++) {
-      const comment = {
-        id: j + 1 + (i - 1) * 30,
-        avatar: `img/avatar-${getRandomInt(1, 6)}.svg`,
-        message: getRandomComments(),
-        name: `Пользователь ${getRandomInt(1, 100)}`
-      };
-      photo.comments.push(comment);
-    }
-    photos.push(photo); // Добавляем фото в массив
+    photos.push(photo);
   }
-
-  // Выводим результаты в консоль
-  // console.log(photos);
+  return photos;
 }
 
-// Запускаем создание объектов фотографий
-createPhotoObjects();
-
+// Запускаем создание объектов фотографий и выводим результат
+console.log(createPhotoObjects());
